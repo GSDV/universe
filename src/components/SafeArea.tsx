6ec2a3c541878f75a6e0665091c612fb9e64dc1e
GSDV <sdvgab@gmail.com>
2024-10-20 12:34:24 -0500

    Custom safe areas

---
+++ src/components/SafeArea.tsx
@@ -0,0 +1,31 @@
+import { View } from 'react-native';
+import { useSafeAreaInsets } from 'react-native-safe-area-context';
+
+
+
+export function SafeAreaFull({ children }: { children: React.ReactNode }) {
+    return (
+        <View style={{ flex: 1 }}>
+            <SafeAreaTop/>
+                <View style={{ flex: 1 }}>
+                    {children}
+                </View>
+            <SafeAreaBottom/>
+        </View>
+    );
+}
+
+
+
+// SafeAreaTop and SafeAreaBottom are to be used independently as well
+// SafeAreaBottom should almost always be used within a parent container of style "flex: 1" and a sibling with "flex: 1", so that it is pushed to the bottom
+
+export function SafeAreaTop() {
+    const insets = useSafeAreaInsets();
+    return <View style={{ paddingTop: insets.top, paddingLeft: insets.left, paddingRight: insets.right }} />;
+}
+
+export function SafeAreaBottom() {
+    const insets = useSafeAreaInsets();
+    return <View style={{ paddingLeft: insets.left, paddingRight: insets.right, paddingBottom: insets.bottom }} />;
+}
