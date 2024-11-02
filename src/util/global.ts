a515e4584607b605384db4e246c7e765363cccd8
GSDV <sdvgab@gmail.com>
2024-11-02 12:49:36 -0500

    Globally shared variables

---
+++ src/util/global.ts
@@ -0,0 +1,40 @@
+// This file must be identical to the server repo's 'src/util/global.ts'
+
+
+
+export const DOMAIN = 'http://localhost:3005';
+export const BRAND = 'IlliniTalk';
+export const CONTACT_EMAIL = 'contact@illininotes.com';
+
+
+
+// COOKIES
+export const AUTH_TOKEN_COOKIE_KEY = 'auth_token';
+export const USER_ID_COOKIE_KEY = 'id_token';
+
+
+
+// USER RESTRICTIONS
+export const MIN_USERNAME_LENGTH = 4;
+export const MAX_USERNAME_LENGTH = 25;
+
+
+
+// POST RESTRICTIONS
+export const MIN_POST_CONTENT_LENGTH = 2;
+export const MAX_POST_CONTENT_LENGTH = 200;
+export const MAX_POST_MEDIA = 4;
+
+export const ACCEPTED_IMGS = ['image/png', 'image/jpeg', 'image/jpg', 'image/webp'];
+export const ACCEPTED_VIDS = ['video/mp4', 'video/quicktime'];
+export const ACCEPTED_FILES = [...ACCEPTED_IMGS, ...ACCEPTED_VIDS];
+export const IMG_SIZE_LIMIT = 15 * 1000000; // 15mb;
+export const IMG_SIZE_LIMIT_TXT = `15mb`;
+export const VID_SIZE_LIMIT = 15 * 1000000; // 15mb;
+export const VID_SIZE_LIMIT_TXT = `15mb`;
+
+
+
+// Media Links
+export const mediaKeyPrefix = (authorId: string) => `users/${authorId}/posts/`;
+export const mediaUrl = (mediaKey: string) => `https://uni-verse.s3.us-east-2.amazonaws.com/${mediaKey}`;
