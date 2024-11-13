51fbf836d3d45f27db8cdd5c4c2d62e1494eda80
GSDV <sdvgab@gmail.com>
2024-11-13 14:38:42 -0600

    Removed unecessary display-flex and direction-column fields

---
+++ src/components/GoBackHeader.tsx
@@ -0,0 +1,24 @@
+import { TouchableOpacity, View } from 'react-native';
+
+import { useRouter } from 'expo-router';
+
+import { Ionicons } from '@expo/vector-icons';
+
+import { SafeAreaTop } from '@components/SafeArea';
+
+import { COLORS } from '@util/global-client';
+
+
+
+export default function GoBackHeader() {
+    const router = useRouter();
+
+    return (
+        <View>
+            <SafeAreaTop />
+            <TouchableOpacity style={{ paddingLeft: 5, width: '100%' }} onPress={router.back}>
+                <Ionicons name='chevron-back' size={25} color={COLORS.primary_1} />
+            </TouchableOpacity>
+        </View>
+    )
+}
