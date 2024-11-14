2080944a679d593c0dfba4e9e624a09bb3482ea1
GSDV <sdvgab@gmail.com>
2024-11-14 15:10:53 -0600

    Shifted OperationProvider to listener-based

--- src/components/providers/OperationProvider.tsx
+++ src/components/providers/OperationProvider.tsx
@@ -1,63 +1,70 @@
-// An operation can be set only at PostView, and only for the parent post.
-// This post can be a reply, but this context is only used on it when it is opened in PostView.
-
-// The operation can be retrieved whenever the screen changes.
-
-// Currently supported operations: LIKE, DELETE
-
 import React, { createContext, useContext, useEffect, useState } from 'react';
+
+import { eventEmitter } from '@util/event';
+
+import { POST_OPERATION_EVENT_KEY } from '@util/global-client';
 
 import { PostType } from '@util/types';
 
 
 
-const OperationContext = createContext<{ 
-    postOp: PostOperationType | null,
-    setOperation: (postId: string, op: OperationType) => void,
+type OperationType = 'LIKE' | 'UNLIKE' | 'DELETE';
+
+interface OperationData {
+    postId: string;
+    op: OperationType;
+}
+
+interface OperationContextType {
+    lastOperation: OperationData | null;
+    emitOperation: (postId: string, op: OperationType) => void;
     conductOperation: (posts: PostType[]) => PostType[];
-}>({ 
-    postOp: null,
-    setOperation: (postId: string, op: OperationType) => {},
+}
+
+
+
+const OperationContext = createContext<OperationContextType>({ 
+    lastOperation: null,
+    emitOperation: (postId: string, op: OperationType) => {},
     conductOperation: (posts: PostType[]) => []
 });
 
 
 
-type OperationType = 'LIKE' | 'UNLIKE' | 'DELETE';
+export function OperationProvider({ children }: { children: React.ReactNode }) {
+    const [lastOperation, setLastOperation] = useState<OperationData | null>(null);
 
-interface PostOperationType {
-    postId: string;
-    op: OperationType;
-}
-
-
-
-export const OperationProvider = ({ children }: { children: React.ReactNode }) => {
-    const [postOp, setPostOp] = useState<PostOperationType | null>(null);
-
-    const setOperation = (postId: string, op: OperationType) => {
-        setPostOp({ postId, op });
-    }
-
-    const conductOperation = (posts: PostType[]) => {
-        if (!postOp) return [];
-
-        const op = postOp.op;
-        const postId = postOp.postId;
-        if (op === 'LIKE') return posts.map(p => (p.id === postId) ? {...p, isLiked: true} : p );
-        if (op === 'UNLIKE') return posts.map(p => (p.id === postId) ? {...p, isLiked: false} : p );
-        if (op === 'DELETE') return posts.filter(post => post.id !== postId);
-
-        // Should never happen, but for TypeScript:
-        return [];
-    }
 
     useEffect(() => {
-        setPostOp(null);
+        const subscriber = eventEmitter.addListener(POST_OPERATION_EVENT_KEY, (data: OperationData) => {
+            setLastOperation(data);
+        });
+        return () => subscriber.remove();
     }, []);
 
+
+    const emitOperation = (postId: string, op: OperationType) => {
+        const postOperationData: OperationData = { postId, op };
+        eventEmitter.emit(POST_OPERATION_EVENT_KEY, postOperationData);
+    };
+
+
+    const conductOperation = (posts: PostType[]) => {
+        if (!lastOperation) return posts;
+
+        const postId = lastOperation.postId;   
+        const op = lastOperation.op;
+        if (op === 'LIKE') return posts.map(p => p.id === postId ? { ...p, isLiked: true, likeCount: p.likeCount + 1 } : p);
+        if (op === 'UNLIKE') return posts.map(p => p.id === postId ? { ...p, isLiked: false, likeCount: p.likeCount -1 } : p);
+        if (op === 'DELETE') return posts.filter(p => p.id !== postId);
+
+        // Should never happen, but just for TypeScript:
+        return posts;
+    }
+
+
     return (
-        <OperationContext.Provider value={{ postOp, setOperation, conductOperation }}>
+        <OperationContext.Provider value={{ lastOperation, emitOperation, conductOperation }}>
             {children}
         </OperationContext.Provider>
     );
