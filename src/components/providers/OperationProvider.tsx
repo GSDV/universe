e9cf50725579039b0567339c14242d8c0b6c4c6c
GSDV <sdvgab@gmail.com>
2024-11-23 19:33:48 -0800

    Screen type of focus

--- src/components/providers/OperationProvider.tsx
+++ src/components/providers/OperationProvider.tsx
@@ -1,67 +1,77 @@
 import React, { createContext, useContext, useEffect, useState } from 'react';
 
-import { eventEmitter } from '@util/event';
-
 import { POST_OPERATION_EVENT_KEY } from '@util/global-client';
+
+import { eventEmitter } from '@util/event';
 
 import { PostType } from '@util/types';
 
 
 
-type OperationType = 'LIKE' | 'UNLIKE' | 'DELETE';
+type Screen = 'account_posts' | 'account_replies' | 'search' | 'feed' | 'map' | 'focus';
 
-interface OperationData {
-    postId: string;
-    op: OperationType;
-}
+
+
+type CreateOp = { name: 'CREATE', postData: PostType }
+const processCreate = (posts: PostType[], op: CreateOp) => [op.postData, ...posts];
+
+type LikeOp = { name: 'LIKE', postId: string }
+const processLike = (posts: PostType[], op: LikeOp) => posts.map(p => p.id === op.postId ? { ...p, isLiked: true, likeCount: p.likeCount + 1 } : p);
+
+type UnlikeOp = { name: 'UNLIKE', postId: string }
+const processUnlike = (posts: PostType[], op: UnlikeOp) => posts.map(p => p.id === op.postId ? { ...p, isLiked: false, likeCount: p.likeCount - 1 } : p);
+
+type DeleteOp = { name: 'DELETE', postId: string }
+const processDelete = (posts: PostType[], op: DeleteOp) => posts.filter(p => p.id !== op.postId);
+
+type BlockOp = { name: 'BLOCK', userId: string }
+const processBlock = (posts: PostType[], op: BlockOp) => posts.filter(p => p.author.id !== op.userId);
+
+
+
+type OperationType = CreateOp | LikeOp | UnlikeOp | DeleteOp | BlockOp;
+
+
 
 interface OperationContextType {
-    lastOperation: OperationData | null;
-    emitOperation: (postId: string, op: OperationType) => void;
-    conductOperation: (posts: PostType[]) => PostType[];
+    lastOperation: OperationType | null;
+    emitOperation: (op: OperationType) => void;
+    conductOperation: (posts: PostType[], screen: Screen) => PostType[];
 }
 
 
 
 const OperationContext = createContext<OperationContextType>({ 
     lastOperation: null,
-    emitOperation: (postId: string, op: OperationType) => {},
-    conductOperation: (posts: PostType[]) => []
+    emitOperation: (op: OperationType) => {},
+    conductOperation: (posts: PostType[], screen: Screen) => []
 });
 
 
 
 export function OperationProvider({ children }: { children: React.ReactNode }) {
-    const [lastOperation, setLastOperation] = useState<OperationData | null>(null);
-
+    const [lastOperation, setLastOperation] = useState<OperationType | null>(null);
 
     useEffect(() => {
-        const subscriber = eventEmitter.addListener(POST_OPERATION_EVENT_KEY, (data: OperationData) => {
-            setLastOperation(data);
+        const subscriber = eventEmitter.addListener(POST_OPERATION_EVENT_KEY, (op: OperationType) => {
+            setLastOperation(op);
         });
         return () => subscriber.remove();
     }, []);
 
+    const emitOperation = (op: OperationType) => eventEmitter.emit(POST_OPERATION_EVENT_KEY, op);
 
-    const emitOperation = (postId: string, op: OperationType) => {
-        const postOperationData: OperationData = { postId, op };
-        eventEmitter.emit(POST_OPERATION_EVENT_KEY, postOperationData);
-    };
-
-
-    const conductOperation = (posts: PostType[]) => {
+    const conductOperation = (posts: PostType[], screen: Screen) => {
         if (!lastOperation) return posts;
+        const opName = lastOperation.name;
 
-        const postId = lastOperation.postId;   
-        const op = lastOperation.op;
-        if (op === 'LIKE') return posts.map(p => p.id === postId ? { ...p, isLiked: true, likeCount: p.likeCount + 1 } : p);
-        if (op === 'UNLIKE') return posts.map(p => p.id === postId ? { ...p, isLiked: false, likeCount: p.likeCount -1 } : p);
-        if (op === 'DELETE') return posts.filter(p => p.id !== postId);
-
-        // Should never happen, but just for TypeScript:
+        if (opName === 'CREATE' && screen === 'account_posts') return processCreate(posts, lastOperation);
+        if (opName === 'LIKE')  return processLike(posts, lastOperation);
+        if (opName === 'UNLIKE') return processUnlike(posts, lastOperation);
+        if (opName === 'DELETE') return processDelete(posts, lastOperation);
+        if (opName === 'BLOCK') return processBlock(posts, lastOperation);
         return posts;
     }
-
 
     return (
         <OperationContext.Provider value={{ lastOperation, emitOperation, conductOperation }}>
