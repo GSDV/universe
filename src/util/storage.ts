c39b739442100842cbd5ed6266fc533b59b620c7
GSDV <sdvgab@gmail.com>
2024-12-10 11:54:42 -0600

    Removing unused UserId Cookie

--- src/util/storage.ts
+++ src/util/storage.ts
@@ -1,6 +1,6 @@
 import AsyncStorage from '@react-native-async-storage/async-storage';
 
-import { AUTH_TOKEN_COOKIE_KEY, USER_ID_COOKIE_KEY } from './global';
+import { AUTH_TOKEN_COOKIE_KEY } from './global';
 
 
 
@@ -11,15 +11,4 @@ export const getAuthCookie = async () => {
 
 export const setAuthCookie = async (val: string) => {
     await AsyncStorage.setItem(AUTH_TOKEN_COOKIE_KEY, val);
-}
-
-
-
-export const getUserIdCookie = async () => {
-    const userId = await AsyncStorage.getItem(USER_ID_COOKIE_KEY);
-    return userId;
-}
-
-export const setUserIdCookie = async (val: string) => {
-    await AsyncStorage.setItem(USER_ID_COOKIE_KEY, val);
 }
