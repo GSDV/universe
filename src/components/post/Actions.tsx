e3c597330c7885d80c3e1863484938088755db4b
GSDV <sdvgab@gmail.com>
2024-12-31 12:26:42 -0800

    Using action util now

--- src/components/post/Actions.tsx
+++ src/components/post/Actions.tsx
@@ -1,5 +1,5 @@
 
-import { ActionSheetIOS, Alert as AlertPopUp } from 'react-native';
+import { Alert as AlertPopUp } from 'react-native';
 
 import { useRouter } from 'expo-router';
 
@@ -11,62 +11,66 @@ import { MAX_REPORT_LENGTH } from '@util/global';
 import { COLORS } from '@util/global-client';
 
 import { fetchWithAuth } from '@util/fetch';
+import { showActionSheet } from '@util/action';
+
+import { PostType } from '@util/types';
 
 
 
 interface PostActionsMenuProps {
-    postId: string;
+    post: PostType;
     ownPost?: boolean;
+    morePostsAvailable?: boolean;
 }
 
-export default function PostActionsMenu({ postId, ownPost }: PostActionsMenuProps) {
+export default function PostActionsMenu({ post, ownPost, morePostsAvailable = true }: PostActionsMenuProps) {
     const router = useRouter();
     const operationContext = useOperation();
 
-    const deletePost = async () => {
-        // Async call
-        fetchWithAuth(`post/${postId}`, 'DELETE');
-        operationContext.emitOperation({ name: 'DELETE', postId: postId });
-        router.back();
+    const handlePinChange = async () => {
+        const body = JSON.stringify({ postId: post.id, pin: !post.pinned });
+        fetchWithAuth(`post/pin`, 'PUT', body);
+        operationContext.emitOperation({ name: 'PIN', selectedPostId: post.id, morePostsAvailable })
     }
 
-    const handleReport = async (reportText: string | undefined) => {
-        AlertPopUp.alert('Post Reported', 'Your report has been sent.', [{ text: 'OK', onPress: () => {} }]);
-        if (!reportText || reportText.length > MAX_REPORT_LENGTH) return;
-        // Async call
-        const body = JSON.stringify({ reportText });
-        fetchWithAuth(`post/${postId}/report`, 'POST', body);
+    const handleDelete = () => {
+        AlertPopUp.alert('Delete post?', 'This cannot be undone.', [
+            { text: 'Cancel', onPress: () => {}, style: 'cancel' },
+            { text: 'Delete', onPress: async () => {
+                // Async call
+                fetchWithAuth(`post/${post.id}`, 'DELETE');
+                operationContext.emitOperation({ name: 'DELETE', postId: post.id });
+                router.back();
+            }, style: 'destructive' },
+        ]);
+    }
+
+    const handleReport = () => {
+        AlertPopUp.prompt('Report Post', 'Please describe why you are reporting this post:', [
+            { text: 'Cancel', onPress: () => {}, style: 'cancel' },
+            { text: 'Send', onPress: (reportText: string | undefined) => {
+                AlertPopUp.alert('Post Reported', 'Your report has been sent.', [{ text: 'OK', onPress: () => {} }]);
+                if (!reportText || reportText.length > MAX_REPORT_LENGTH) return;
+                // Async call
+                const body = JSON.stringify({ reportText });
+                fetchWithAuth(`post/${post.id}/report`, 'POST', body);
+            } }
+        ], 'plain-text');
     }
 
 
     const ownOptions = () => {
-        ActionSheetIOS.showActionSheetWithOptions(
-            { options: ['Cancel', 'Delete'], cancelButtonIndex: 0, destructiveButtonIndex: 1 },
-            async (buttonIndex) => {
-                if (buttonIndex == 0) { } 
-                else if (buttonIndex == 1) {
-                    AlertPopUp.alert('Delete post?', 'This cannot be undone.', [
-                        { text: 'Cancel', onPress: () => {}, style: 'cancel' },
-                        { text: 'Delete', onPress: deletePost, style: 'destructive' },
-                    ]);
-                }
-            }
-        );
+        const options = [{ label: 'Delete', action: handleDelete }];
+        if (post.replyToId !== '') {
+            const label = (post.pinned) ? 'Unpin' : 'Pin';
+            options.push({ label, action: handlePinChange });
+        }
+        showActionSheet(options, [0]);
     }
 
     const otherOptions = () => {
-        ActionSheetIOS.showActionSheetWithOptions(
-            { options: ['Cancel', 'Report'], cancelButtonIndex: 0, destructiveButtonIndex: 1 },
-            (buttonIndex) => {
-                if (buttonIndex == 0) { } 
-                else if (buttonIndex == 1) {
-                    AlertPopUp.prompt('Report Post', 'Please describe why you are reporting this post:', [
-                        { text: 'Cancel', onPress: () => {}, style: 'cancel' },
-                        { text: 'Send', onPress: handleReport }
-                    ], 'plain-text');
-                }
-            }
-        );
+        const options = [{ label: 'Report', action: handleReport }];
+        showActionSheet(options, [0]);
     }
 
     const onEllipsisPres = ownPost ? ownOptions : otherOptions;
