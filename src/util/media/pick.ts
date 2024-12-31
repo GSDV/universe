c3b07d4b72737782ddd4d936293ff217060b7b30
GSDV <sdvgab@gmail.com>
2024-12-31 12:23:50 -0800

    Moving s3

---
+++ src/util/media/pick.ts
@@ -0,0 +1,89 @@
+
+import * as ImagePicker from 'expo-image-picker';
+import * as ImageManipulator from 'expo-image-manipulator';
+
+import { ACCEPTED_FILES, ACCEPTED_IMGS, ACCEPTED_VIDS, IMG_SIZE_LIMIT, IMG_SIZE_LIMIT_TXT, VID_SIZE_LIMIT, VID_SIZE_LIMIT_TXT } from '@util/global';
+
+
+
+export const getMedia = async (selectionLimit: number) => {
+    try {
+        const result = await ImagePicker.launchImageLibraryAsync({
+            mediaTypes: ImagePicker.MediaTypeOptions.All,
+            allowsMultipleSelection: true,
+            orderedSelection: true,
+            selectionLimit,
+            quality: 0.8,
+            videoMaxDuration: 60
+        });
+        if (!result || result.canceled) {
+            return {
+                optimizedMedia: [], 
+                resp: {
+                    msg: `User canceled.`,
+                    cStatus: 200
+                }
+            };
+        }
+
+        const assets = result.assets.filter((asset: any) => asset.mimeType !== undefined);
+
+        for (let i = 0; i < assets.length; i++) {
+            const asset = assets[i];
+            const type = asset.mimeType;
+            if (type==undefined || !ACCEPTED_FILES.includes(type)) {
+                return {
+                    optimizedMedia: null, 
+                    resp: {
+                        msg: `Please only upload png, jpg, webp, mp4, or mov files.`,
+                        cStatus: 400
+                    }
+                };
+            }
+            if (ACCEPTED_IMGS.includes(asset.type as string) && (asset.fileSize ? asset.fileSize : 0) > IMG_SIZE_LIMIT) {
+                return {
+                    optimizedMedia: null, 
+                    resp: {
+                        msg: `Please upload pictures under ${IMG_SIZE_LIMIT_TXT}.`,
+                        cStatus: 400
+                    }
+                };
+            }
+            if (ACCEPTED_VIDS.includes(asset.type as string) && (asset.fileSize ? asset.fileSize : 0) > VID_SIZE_LIMIT) {
+                return {
+                    optimizedMedia: null, 
+                    resp: { msg: `Please upload videos under ${VID_SIZE_LIMIT_TXT}.`, cStatus: 400 }
+                };
+            }
+        }
+
+        const optimizedMedia = await Promise.all(
+            assets.map(async (asset) => {
+                const isVideo = asset.type === 'video';
+    
+                if (isVideo) {
+                    // No compression for now
+                    return { uri: asset.uri, type: (asset.mimeType as string) };
+                } else {
+                    const optimized = await ImageManipulator.manipulateAsync(
+                        asset.uri,
+                        [{ resize: { width: 1080 } }],
+                        { compress: 0.8, format: ImageManipulator.SaveFormat.JPEG }
+                    );
+
+                    return { uri: optimized.uri, type: (asset.mimeType as string) };
+                }
+            })
+        );
+
+        return {
+            optimizedMedia, 
+            resp: { msg: `Success.`, cStatus: 200 }
+        };
+    } catch (err) {
+        return {
+            optimizedMedia: null, 
+            resp: { msg: `Something went wrong while uploading media.`, cStatus: 400 }
+        };
+    }
+}
--- src/util/media/s3.ts
+++ src/util/media/s3.ts
@@ -6,7 +6,9 @@ import { Alert as AlertPopUp, Linking } from 'react-native'
 
 import * as ImagePicker from 'expo-image-picker';
 
-import { fetchWithAuth } from './fetch';
+import { fetchWithAuth } from '../fetch';
+import { UploadedAsset } from '@components/post/media/Display';
+import { IMG_SIZE_LIMIT_TXT, VID_SIZE_LIMIT_TXT } from '@util/global';
 
 
 
@@ -27,8 +29,45 @@ export const promptMediaPermissions = async () => {
 
 
 
-export const clientUploadMediaAndGetKeys = async (media: Blob[]) => {
-    const keyPromises = media.map((asset) => clientUploadMedia(asset));
+export const getMediaKeys = async (media: UploadedAsset[]) => {
+    const blobPromises = media.map(async (asset) => {
+        try {
+            const response = await fetch(asset.uri);
+            const blob = await response.blob();
+            return blob;
+        } catch (error) {
+            return null;
+        }
+    });
+    const resBlobs = await Promise.all(blobPromises);
+
+    if (resBlobs.includes(null)) {
+        return {
+            mediaKeys: null,
+            resp: { msg: `Something went wrong while uploading media.`, cStatus: 400 }
+        };
+    }
+
+    // For TypeScript:
+    const filteredBlobs = resBlobs.filter(blob => blob !== null);
+
+    const mediaKeys = await clientUploadMediaAndGetKeys(filteredBlobs);
+    if (!mediaKeys || mediaKeys.includes(null)) {
+        return {
+            mediaKeys: null,
+            resp: { msg: `Please upload photos under ${IMG_SIZE_LIMIT_TXT} and videos under ${VID_SIZE_LIMIT_TXT}.`, cStatus: 400 }
+        };
+    }
+    return {
+        mediaKeys: mediaKeys as string[],
+        resp: { msg: `Success.`, cStatus: 200 }
+    };
+}
+
+
+
+export const clientUploadMediaAndGetKeys = async (blobs: Blob[]) => {
+    const keyPromises = blobs.map((blob) => clientUploadMedia(blob));
     const imageKeys = await Promise.all(keyPromises);
 
     if (imageKeys.includes(null)) return null;
@@ -37,19 +76,19 @@ export const clientUploadMediaAndGetKeys = async (media: Blob[]) => {
 
 
 
-export const clientUploadMedia = async (asset: Blob) => {
-    const body = JSON.stringify({ fileType: asset.type, fileSize: asset.size });
+export const clientUploadMedia = async (blob: Blob) => {
+    const body = JSON.stringify({ fileType: blob.type, fileSize: blob.size });
     const resSignAndKeyJson = await fetchWithAuth(`post/s3`, 'POST', body);
     if (resSignAndKeyJson.cStatus!=200) return null;
 
-    const assetBlob = new Blob([asset], { type: asset.type });
+    const assetBlob = new Blob([blob], { type: blob.type });
 
     const resS3 = await fetch(resSignAndKeyJson.signedUrl, {
         method: 'PUT',
         body: assetBlob
     });
 
-    return (resS3.ok) ? resSignAndKeyJson.key : null;
+    return (resS3.ok) ? resSignAndKeyJson.key as string : null;
 }
 
 
