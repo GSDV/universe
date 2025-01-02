fb73fc917033faa2212d3db0505b0e79b4b37bf4
GSDV <sdvgab@gmail.com>
2025-01-02 14:04:42 -0800

    Profile view screen

---
+++ app/profile/[username]/view.tsx
@@ -0,0 +1,57 @@
+import { useEffect, useState } from 'react';
+
+import { View } from 'react-native';
+
+import { useLocalSearchParams } from 'expo-router';
+
+import Acccount from '@screens/account/Account';
+
+import { AlertType, CheckIfAlert } from '@components/Alert';
+import { CheckIfLoading } from '@components/Loading';
+import GoBackHeader from '@components/GoBackHeader';
+
+import { fetchWithAuth } from '@util/fetch';
+
+import { RedactedUserTypeWithFollow } from '@util/types';
+
+
+
+export default function Index() {
+    const { username } = useLocalSearchParams();
+
+    const [user, setUser] = useState<RedactedUserTypeWithFollow>();
+    const [ownAccount, setOwnAccount] = useState<boolean>(false);
+
+    const [loading, setLoading] = useState<boolean>(true);
+    const [alert, setAlert] = useState<AlertType>();
+
+    const fetchAccount = async () => {
+        setLoading(true);
+
+        const resJson = await fetchWithAuth(`profile/${username}`, 'GET');
+
+        if (resJson.cStatus == 200) {
+            setUser(resJson.user);
+            setOwnAccount(resJson.ownAccount);
+        } else {
+            setAlert(resJson);
+        }
+
+        setLoading(false);
+    }
+
+    useEffect(() => {
+        fetchAccount();
+    }, []);
+
+    return (
+        <View style={{ flex: 1 }}>
+            <GoBackHeader />
+            <CheckIfLoading loading={loading}>
+                <CheckIfAlert alert={alert}>
+                    <Acccount userPrisma={user as RedactedUserTypeWithFollow} ownAccount={ownAccount} />
+                </CheckIfAlert>
+            </CheckIfLoading>
+        </View>
+    );
+}
