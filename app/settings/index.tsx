32f9b1a22cfe561f2727523b81b8c620f36d7141
GSDV <sdvgab@gmail.com>
2025-01-01 18:04:25 -0800

    Delete account route implement

--- app/settings/index.tsx
+++ app/settings/index.tsx
@@ -1,20 +1,26 @@
-import { View, Text, TouchableOpacity, ScrollView, StyleSheet } from 'react-native';
+import { useState } from 'react';
+
+import { View, Text, TouchableOpacity, ScrollView, StyleSheet, Alert as AlertPopUp } from 'react-native';
 
 import { useRouter } from 'expo-router';
 
 import { useUser } from '@components/providers/UserProvider';
 
 import GoBackHeader from '@components/GoBackHeader';
+import { CheckIfLoading } from '@components/Loading';
 
 import { COLORS, FONT_SIZES } from '@util/global-client';
 
 import { setAuthCookie } from '@util/storage';
+import { fetchWithAuth } from '@util/fetch';
 
 
 
 export default function Index() {
     const userContext = useUser();
     const router = useRouter();
+
+    const [loading, setLoading] = useState<boolean>(false);
 
     const attemptLogout = async () => {
         // Must navigate BEFORE resetting userContext and AuthCookie.
@@ -24,23 +30,58 @@ export default function Index() {
         await setAuthCookie('');
     }
 
+
+    const promptDelete = () => {
+        AlertPopUp.prompt('Type "DELETE" to delete your account.', 'This action cannot be undone.', [
+            { text: 'Cancel', onPress: () => {}, style: 'cancel' },
+            { text: 'Send', onPress: async (text: string | undefined) => {
+                if (text === undefined || text.trim().toUpperCase() !== 'DELETE') return;
+
+                setLoading(true);
+                // Async call
+                await (new Promise(resolve => setTimeout(resolve, 2000)));
+
+                const userId = userContext.user?.id ?? '';
+                const body = JSON.stringify({ userId });
+                const resJson = await fetchWithAuth(`user/${userId}`, 'DELETE', body);
+
+                if (resJson.cStatus == 200) {
+                    attemptLogout();
+                    AlertPopUp.alert('You account has been deleted.', '', [{ text: 'OK', onPress: () => {} }]);
+                } else {
+                    AlertPopUp.alert('Something went wrong.', '', [{ text: 'OK', onPress: () => {} }]);
+                }
+                setLoading(false);
+            } }
+        ], 'plain-text');
+    }
+
+    const askDelete = () => {
+        AlertPopUp.alert('Delete Account', 'Are you sure you want to delete your account?', [
+            { text: 'No', style: 'default', onPress: () => {} },
+            { text: 'Yes', style: 'destructive', onPress: promptDelete }
+        ]);
+    }
+
     return (
         <ScrollView contentContainerStyle={{ flex: 1 }}>
             <GoBackHeader />
             <View style={styles.container}>
                 <Text style={styles.title} >Settings</Text>
 
-                <TouchableOpacity onPress={() => router.push('/settings/profile-edit')}>
-                    <Text style={styles.buttonText}>Edit Profile</Text>
-                </TouchableOpacity>
+                <CheckIfLoading loading={loading}>
+                    <TouchableOpacity onPress={() => router.push('/settings/profile-edit')}>
+                        <Text style={styles.buttonText}>Edit Profile</Text>
+                    </TouchableOpacity>
 
-                <TouchableOpacity onPress={attemptLogout}>
-                    <Text style={styles.buttonText}>Logout</Text>
-                </TouchableOpacity>
+                    <TouchableOpacity onPress={attemptLogout}>
+                        <Text style={styles.buttonText}>Logout</Text>
+                    </TouchableOpacity>
 
-                <TouchableOpacity onPress={() => console.log("Prompt delete account")}>
-                    <Text style={styles.buttonText}>Delete Account</Text>
-                </TouchableOpacity>
+                    <TouchableOpacity onPress={askDelete}>
+                        <Text style={styles.buttonText}>Delete Account</Text>
+                    </TouchableOpacity>
+                </CheckIfLoading>
             </View>
         </ScrollView>
     );
