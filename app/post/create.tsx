1071c18a017107e9ae1dc8869a55506f3d0986f4
GSDV <sdvgab@gmail.com>
2025-01-04 15:32:31 -0800

    Remove commented out code

--- app/post/create.tsx
+++ app/post/create.tsx
@@ -1,14 +1,12 @@
-import { useEffect, useState } from 'react';
 import { View } from 'react-native';
-
-import CreatePostScreen from '@screens/post/Create';
 
 import { useUser } from '@components/providers/UserProvider';
 
+import CreatePostScreen from '@screens/post/Create';
 import { SafeAreaFull } from '@components/SafeArea';
-import { CheckIfLoading } from '@components/Loading';
 
 import { COLORS } from '@util/global-client';
+
 import { RedactedUserType } from '@util/types';
 
 
@@ -16,25 +14,10 @@ import { RedactedUserType } from '@util/types';
 export default function Index() {
     const userContext = useUser();
 
-    // const [loading, setLoading] = useState<boolean>(false);
-    // const [userPrisma, setUserPrisma] = useState<RedactedUserType | null>(userContext.user);
-
-    // const fetchaccount = async () => {
-    //     setLoading(true);
-    //     setUserPrisma(userContext.user);
-    //     setLoading(false);
-    // }
-
-    // useEffect(() => {
-    //     fetchaccount();
-    // }, []);
-
     return (
         <View style={{ flex: 1, backgroundColor: COLORS.background }}>
             <SafeAreaFull>
-            {/* <CheckIfLoading loading={loading}> */}
-                {userContext.user && <CreatePostScreen userPrisma={userContext.user as RedactedUserType} />}
-            {/* </CheckIfLoading> */}
+                <CreatePostScreen userPrisma={userContext.user as RedactedUserType} />
             </ SafeAreaFull>
         </View>
     );
