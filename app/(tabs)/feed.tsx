ef50c968f0c23a9b0226e4ae16f33df9c3a993d9
GSDV <sdvgab@gmail.com>
2025-01-05 15:02:53 -0800

    Feed view

--- app/(tabs)/feed.tsx
+++ app/(tabs)/feed.tsx
@@ -1,28 +1,16 @@
-import { StyleSheet } from 'react-native';
-import { View, Text } from 'react-native';
-import { Link } from 'expo-router';
-import { useEffect, useState } from 'react';
+import { View } from 'react-native';
+
+import Feed from '@screens/feed/Feed';
+
+import { SafeAreaTop } from '@components/SafeArea';
 
 
 
 export default function Index() {
-    // This is for when we implement followers and followed.
-    // const [loading, setLoading] = useState<boolean>(false);
-    // const getStoredUser = async () => {
-    //     await fetchStoredUser();
-    // }
-
-    const fetchFeedPosts = async () => {
-        const res = await fetch('SERVER');
-        const resJson = await res.json();
-        if (resJson.cStatus == 200) console.log("add to usetstae");//;
-    }
-    useEffect(() => {
-        
-    })
-
     return (
-        <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+        <View style={{ flex: 1 }}>
+            <SafeAreaTop />
+            <Feed />
         </View>
     );
 }
---
+++ src/screens/feed/Feed.tsx
@@ -0,0 +1,137 @@
+import { useEffect, useState } from 'react';
+
+import { View, Text, TouchableOpacity } from 'react-native';
+
+import { FeedPost } from '@components/post/FeedPost';
+import List from '@components/List';
+import { CheckIfLoading } from '@components/Loading';
+
+import { COLORS, FONT_SIZES } from '@util/global-client';
+
+import { fetchWithAuth } from '@util/fetch';
+
+import { PostType } from '@util/types';
+
+
+
+export default function Feed() {
+    const [view, setView] = useState<'hot' | 'new' | 'school'>('hot');
+
+    const [loading, setLoading] = useState<boolean>(true);
+
+    const [hotPosts, setHotPosts] = useState<PostType[]>([]);
+    const [hotCursor, setHotCursor] = useState<string>('');
+    const [moreHotAvailable, setMoreHotAvailable] = useState<boolean>(false);
+
+    const [newPosts, setNewPosts] = useState<PostType[]>([]);
+    const [newCursor, setNewCursor] = useState<string>('');
+    const [moreNewAvailable, setMoreNewAvailable] = useState<boolean>(false);
+
+    const [schoolPosts, setSchoolPosts] = useState<PostType[]>([]);
+    const [schoolCursor, setSchoolCursor] = useState<string>('');
+    const [moreSchoolAvailable, setMoreSchoolAvailable] = useState<boolean>(false);
+
+    const fetchAndUpdateHot = async (cursor: string, oldPosts: PostType[]) => {
+        const params = new URLSearchParams({ cursor });
+        const resJson = await fetchWithAuth(`feed/hot?${params.toString()}`, 'GET');
+        if (resJson.cStatus == 200) {
+            setHotCursor(resJson.nextCursor);
+            setHotPosts([...oldPosts, ...resJson.posts]);
+            setMoreHotAvailable(resJson.moreAvailable);
+        }
+    }
+
+    const fetchAndUpdateNew = async (cursor: string, oldPosts: PostType[]) => {
+        const params = new URLSearchParams({ cursor });
+        const resJson = await fetchWithAuth(`feed/new?${params.toString()}`, 'GET');
+        if (resJson.cStatus == 200) {
+            setNewCursor(resJson.nextCursor);
+            setNewPosts([...oldPosts, ...resJson.posts]);
+            setMoreNewAvailable(resJson.moreAvailable);
+        }
+    }
+
+    const fetchAndUpdateSchool = async (cursor: string, oldPosts: PostType[]) => {
+        const params = new URLSearchParams({ cursor });
+        const resJson = await fetchWithAuth(`feed/school?${params.toString()}`, 'GET');
+        if (resJson.cStatus == 200) {
+            setSchoolCursor(resJson.nextCursor);
+            setSchoolPosts([...oldPosts, ...resJson.posts]);
+            setMoreSchoolAvailable(resJson.moreAvailable);
+        }
+    }
+
+    const renderPost = (post: PostType) => <FeedPost post={post} />;
+
+    const intialFetch = async () => {
+        setLoading(true);
+        await Promise.all([
+            fetchAndUpdateNew('', []),
+            fetchAndUpdateHot('', []),
+            fetchAndUpdateSchool('', [])
+        ]);
+        setLoading(false);
+    }
+
+    useEffect(() => {
+        intialFetch();
+    }, []);
+
+    return (
+        <>
+            <View style={{ paddingVertical: 15, width: '100%', flexDirection: 'row', justifyContent: 'center', gap: 15 }}>
+                <TouchableOpacity onPress={() => setView('hot')}>
+                    <Text style={{ color: ((view==='hot') ? COLORS.black : COLORS.gray), fontSize: FONT_SIZES.m }}>Hot</Text>
+                </TouchableOpacity>
+
+                <Text style={{ color: COLORS.gray, fontSize: FONT_SIZES.m }}>|</Text>
+
+                <TouchableOpacity onPress={() => setView('new')}>
+                    <Text style={{ color: ((view==='new') ? COLORS.black : COLORS.gray), fontSize: FONT_SIZES.m }}>New</Text>
+                </TouchableOpacity>
+
+                <Text style={{ color: COLORS.gray, fontSize: FONT_SIZES.m }}>|</Text>
+
+                <TouchableOpacity onPress={() => setView('school')}>
+                    <Text style={{ color: ((view==='school') ? COLORS.black : COLORS.gray), fontSize: FONT_SIZES.m }}>My School</Text>
+                </TouchableOpacity>
+            </View>
+
+            <View style={{ width: '100%', height: 5, backgroundColor: 'rgb(220, 220, 220)' }} />
+
+            <CheckIfLoading loading={loading}>
+                <>{
+                    {
+                    'hot':
+                        <List<PostType> 
+                            items={hotPosts} 
+                            cursor={hotCursor} 
+                            moreAvailable={moreHotAvailable} 
+                            fetchAndUpdate={fetchAndUpdateHot} 
+                            renderItem={renderPost}
+                            noResultsText='no posts yet'
+                        />,
+                    'new':
+                        <List<PostType> 
+                            items={newPosts} 
+                            cursor={newCursor} 
+                            moreAvailable={moreNewAvailable} 
+                            fetchAndUpdate={fetchAndUpdateNew} 
+                            renderItem={renderPost}
+                            noResultsText='no posts yet'
+                        />,
+                    'school':
+                        <List<PostType> 
+                            items={schoolPosts} 
+                            cursor={schoolCursor} 
+                            moreAvailable={moreSchoolAvailable} 
+                            fetchAndUpdate={fetchAndUpdateSchool} 
+                            renderItem={renderPost}
+                            noResultsText='no posts yet'
+                        />
+                    }[view]
+                }</>
+            </CheckIfLoading>
+        </>
+    );
+}
