6087eb070f1e2b51074e651e17071d087c8bf3d6
GSDV <sdvgab@gmail.com>
2025-01-04 18:17:55 -0800

    Fixed invalid cursor

--- app/(tabs)/search.tsx
+++ app/(tabs)/search.tsx
@@ -1,75 +1,261 @@
-import { Image, StyleSheet, Platform, Text } from 'react-native';
+import { useEffect, useState } from 'react';
 
-export default function HomeScreen() {
+import { TextInput, View, Text, Platform, StatusBar, Keyboard, StyleSheet, TouchableOpacity, TouchableWithoutFeedback } from 'react-native';
+
+import { useUser } from '@components/providers/UserProvider';
+
+import { Ionicons, Feather, MaterialCommunityIcons } from '@expo/vector-icons';
+
+import { FeedPost } from '@components/post/FeedPost';
+import { SafeAreaTop } from '@components/SafeArea';
+import { CheckIfLoading } from '@components/Loading';
+
+import { COLORS, FONT_SIZES } from '@util/global-client';
+
+import { fetchWithAuth } from '@util/fetch';
+
+import { PostType, RedactedUserWithFollow } from '@util/types';
+import { useRouter } from 'expo-router';
+import Pfp from '@components/Pfp';
+import List from '@components/List';
+
+
+
+export default function Index() {
+    const [input, setInput] = useState<string>('');
+    const [query, setQuery] = useState<string>('');
+
+    const onSubmit = () => setQuery(input.trim());
+
     return (
-        <Text>HOME SCREEN</Text>
-    )
+        <TouchableWithoutFeedback style={{ flex: 1 }} onPress={Keyboard.dismiss}>
+            <View style={{ flex: 1 }}>
+                <SafeAreaTop />
+                <View style={styles.searchContainer}>
+                    <Ionicons name='search' size={20} color={COLORS.gray} style={styles.searchIcon} />
+                    <TextInput
+                        style={styles.searchInput}
+                        placeholder='Search...'
+                        value={input}
+                        onChangeText={setInput}
+                        placeholderTextColor={COLORS.gray}
+                        autoCapitalize='none'
+                        autoCorrect={false}
+                        returnKeyType='search'
+                        onSubmitEditing={onSubmit}
+                    />
+                    {input.trim() && 
+                        <TouchableOpacity onPress={onSubmit}>
+                            <Feather name='send' size={24} color={COLORS.primary_1} />
+                        </TouchableOpacity>
+                    }
+                </View>
+
+                {(query === '') ? 
+                    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+                        <Text style={{ color: COLORS.black, fontSize: FONT_SIZES.s }}>search posts and users</Text>
+                    </View>
+                :
+                    <PostsAndUsers query={query} />
+                }
+            </View>
+        </TouchableWithoutFeedback>
+    );
 }
-// import { HelloWave } from '@/components/HelloWave';
-// import ParallaxScrollView from '@/components/ParallaxScrollView';
-// import { ThemedText } from '@/components/ThemedText';
-// import { ThemedView } from '@/components/ThemedView';
 
-// export default function HomeScreen() {
-//   return (
-//     <ParallaxScrollView
-//       headerBackgroundColor={{ light: '#A1CEDC', dark: '#1D3D47' }}
-//       headerImage={
-//         <Image
-//           source={require('@/assets/images/partial-react-logo.png')}
-//           style={styles.reactLogo}
-//         />
-//       }>
-//       <ThemedView style={styles.titleContainer}>
-//         <ThemedText type="title">Welcome!</ThemedText>
-//         <HelloWave />
-//       </ThemedView>
-//       <ThemedView style={styles.stepContainer}>
-//         <ThemedText type="subtitle">Step 1: Try it</ThemedText>
-//         <ThemedText>
-//           Edit <ThemedText type="defaultSemiBold">app/(tabs)/index.tsx</ThemedText> to see changes.
-//           Press{' '}
-//           <ThemedText type="defaultSemiBold">
-//             {Platform.select({ ios: 'cmd + d', android: 'cmd + m' })}
-//           </ThemedText>{' '}
-//           to open developer tools.
-//         </ThemedText>
-//       </ThemedView>
-//       <ThemedView style={styles.stepContainer}>
-//         <ThemedText type="subtitle">Step 2: Explore</ThemedText>
-//         <ThemedText>
-//           Tap the Explore tab to learn more about what's included in this starter app.
-//         </ThemedText>
-//       </ThemedView>
-//       <ThemedView style={styles.stepContainer}>
-//         <ThemedText type="subtitle">Step 3: Get a fresh start</ThemedText>
-//         <ThemedText>
-//           When you're ready, run{' '}
-//           <ThemedText type="defaultSemiBold">npm run reset-project</ThemedText> to get a fresh{' '}
-//           <ThemedText type="defaultSemiBold">app</ThemedText> directory. This will move the current{' '}
-//           <ThemedText type="defaultSemiBold">app</ThemedText> to{' '}
-//           <ThemedText type="defaultSemiBold">app-example</ThemedText>.
-//         </ThemedText>
-//       </ThemedView>
-//     </ParallaxScrollView>
-//   );
-// }
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        paddingTop: Platform.OS === 'android' ? StatusBar.currentHeight : 0,
+    },
+    searchContainer: {
+        flexDirection: 'row',
+        alignItems: 'center',
+        backgroundColor: '#f0f0f0',
+        marginHorizontal: 16,
+        marginVertical: 8,
+        paddingHorizontal: 12,
+        borderRadius: 20,
+        height: 40,
+    },
+    searchIcon: {
+        marginRight: 8,
+    },
+    searchInput: {
+        flex: 1,
+        fontSize: 16,
+        color: '#333',
+        height: '100%'
+    },
+  });
+  
 
-// const styles = StyleSheet.create({
-//   titleContainer: {
-//     flexDirection: 'row',
-//     alignItems: 'center',
-//     gap: 8,
-//   },
-//   stepContainer: {
-//     gap: 8,
-//     marginBottom: 8,
-//   },
-//   reactLogo: {
-//     height: 178,
-//     width: 290,
-//     bottom: 0,
-//     left: 0,
-//     position: 'absolute',
-//   },
-// });
+
+
+function PostsAndUsers({ query }: { query: string }) {
+    const userContext = useUser();
+
+    const [view, setView] = useState<'posts' | 'users'>('posts');
+
+    const [loading, setLoading] = useState<boolean>(true);
+
+    const [posts, setPosts] = useState<PostType[]>([]);
+    const [postsCursor, setPostsCursor] = useState<Date>(new Date());
+    const [morePostsAvailable, setMorePostsAvailable] = useState<boolean>(false);
+
+    const [users, setUsers] = useState<RedactedUserWithFollow[]>([]);
+    const [usersCursor, setUsersCursor] = useState<Date>(new Date());
+    const [moreUsersAvailable, setMoreUsersAvailable] = useState<boolean>(false);
+
+
+    const fetchAndUpdatePosts = async (cursor: Date, oldPosts: PostType[]) => {
+        const params = new URLSearchParams({
+            query,
+            cursor: cursor.toISOString()
+        });
+        const resJson = await fetchWithAuth(`search/posts?${params.toString()}`, 'GET');
+        if (resJson.cStatus == 200) {
+            setPostsCursor(new Date(resJson.newCursor));
+            setPosts([...oldPosts, ...resJson.posts]);
+            setMorePostsAvailable(resJson.moreAvailable);
+        }
+    }
+
+    const fetchAndUpdateUsers = async (cursor: Date, oldUsers: RedactedUserWithFollow[]) => {
+        const params = new URLSearchParams({
+            query,
+            cursor: cursor.toISOString()
+        });
+        const resJson = await fetchWithAuth(`search/users?${params.toString()}`, 'GET');
+        if (resJson.cStatus == 200) {
+            setUsersCursor(new Date(resJson.newCursor));
+            setUsers([...oldUsers, ...resJson.users]);
+            setMoreUsersAvailable(resJson.moreAvailable);
+        }
+    }
+
+    const renderPost = (post: PostType) => <FeedPost post={post} ownPost={userContext?.user?.id === post.author.id} />;
+
+    const renderUser = (user: RedactedUserWithFollow) => <FeedAccount user={user} />;
+
+    const intialFetch = async () => {
+        setLoading(true);
+        const now = new Date();
+        fetchAndUpdatePosts(now, []);
+        fetchAndUpdateUsers(now, []);
+        setLoading(false);
+    }
+
+    useEffect(() => {
+        intialFetch();
+    }, [query]);
+
+    return (
+        <>
+            <View style={{ paddingVertical: 15, width: '100%', flexDirection: 'row', justifyContent: 'center', gap: 15 }}>
+                <TouchableOpacity onPress={() => setView('posts')}>
+                    <Text style={{ color: ((view==='posts') ? COLORS.black : COLORS.gray), fontSize: FONT_SIZES.m }}>Posts</Text>
+                </TouchableOpacity>
+
+                <Text style={{ color: COLORS.gray, fontSize: FONT_SIZES.m }}>|</Text>
+
+                <TouchableOpacity onPress={() => setView('users')}>
+                    <Text style={{ color: ((view==='users') ? COLORS.black : COLORS.gray), fontSize: FONT_SIZES.m }}>Users</Text>
+                </TouchableOpacity>
+            </View>
+
+            <View style={{ width: '100%', height: 5, backgroundColor: 'rgb(220, 220, 220)' }} />
+
+            <CheckIfLoading loading={loading}>
+                <>
+                    {view === 'posts' ?
+                        <List<PostType> 
+                            items={posts} 
+                            cursor={postsCursor} 
+                            moreAvailable={morePostsAvailable} 
+                            fetchAndUpdate={fetchAndUpdatePosts} 
+                            renderItem={renderPost}
+                            allowRefresh={false}
+                            noResultsText='no posts found'
+                        />
+                    :
+                        <List<RedactedUserWithFollow> 
+                            items={users} 
+                            cursor={usersCursor} 
+                            moreAvailable={moreUsersAvailable} 
+                            fetchAndUpdate={fetchAndUpdateUsers} 
+                            renderItem={renderUser}
+                            allowRefresh={false}
+                            noResultsText='no accounts found'
+                        />
+                    }
+                </>
+            </CheckIfLoading>
+        </>
+    );
+}
+
+
+
+export function FeedAccount({ user }: { user: RedactedUserWithFollow }) {
+    const router = useRouter();
+    const onPress = () => {
+        router.push({
+            pathname: '/profile/[username]/view',
+            params: { username: user.username }
+        });
+    }
+
+    return (
+        <TouchableOpacity onPress={onPress} style={account_feed_styles.container}>
+            <View style={account_feed_styles.header}>
+                <Pfp pfpKey={user.pfpKey} style={account_feed_styles.pfp} />
+
+                <View style={{ flex: 1, justifyContent: 'center', gap: 2 }}>
+                    <View style={{ flexDirection: 'row', alignItems: 'center', gap: 5 }}>
+                        <Text style={account_feed_styles.displayName} numberOfLines={1} ellipsizeMode='tail'>{user.displayName}</Text>
+                        {user.verified && <MaterialCommunityIcons name='star-four-points' style={{ fontSize: FONT_SIZES.m }} color={COLORS.primary_1} />}
+                    </View>
+                    <Text style={account_feed_styles.username} numberOfLines={1} ellipsizeMode='tail'>@{user.username}</Text>
+                </View>
+            </View>
+
+            {user.bio!='' && <Text ellipsizeMode='tail' numberOfLines={1} style={{ color: COLORS.black, fontSize: FONT_SIZES.s }}>{user.bio}</Text>}
+
+            {user.isFollowed && <Text style={{ color: COLORS.gray, fontSize: FONT_SIZES.s }}>Following</Text>}
+        </TouchableOpacity>
+    );
+}
+
+
+
+const account_feed_styles = StyleSheet.create({
+    container: {
+        padding: 15,
+        paddingHorizontal: 20,
+        width: '100%',
+        gap: 10,
+        backgroundColor: COLORS.background
+    },
+    header: {
+        flexDirection: 'row',
+        justifyContent: 'center',
+        gap: 10
+    },
+    pfp: {
+        borderRadius: 50,
+        width: 50,
+        height: 50
+    },
+    displayName: {
+        maxWidth: '50%',
+        color: COLORS.black,
+        fontSize: FONT_SIZES.m,
+        fontWeight: '500'
+    },
+    username: {
+        color: COLORS.gray,
+        fontSize: FONT_SIZES.m
+    }
+});
