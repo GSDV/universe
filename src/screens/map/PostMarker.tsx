cff01c7ab3e8bd6421d5b955ac1cd35b6f7ccd41
GSDV <sdvgab@gmail.com>
2025-01-09 23:10:15 -0800

    Star post marker map

---
+++ src/screens/map/PostMarker.tsx
@@ -0,0 +1,61 @@
+import { StyleSheet, TouchableOpacity } from 'react-native';
+
+import MaterialCommunityIcons from '@expo/vector-icons/MaterialCommunityIcons';
+
+import { COLORS } from '@util/global-client';
+
+import { PostType } from '@util/types';
+
+
+
+export default function PostMarker({ post, onPress }: { post: PostType; onPress: () => void }) {
+    const score = post.likeCount + post.replyCount/3;
+
+    return (
+        <TouchableOpacity onPress={onPress} style={styles.container}>
+            <Star score={score} />
+        </TouchableOpacity>
+    );
+}
+
+
+
+function Star({ score }: { score: number }) {
+    // New/Unpopular posts.
+    // Note: due to some internal padding in specifically the 'star-three-points' component, 
+    //      we must offset one by a few pixels to be centered.
+    if (score < 20) return (
+        <>
+            <MaterialCommunityIcons style={{ zIndex: 1, position: 'absolute' }} name='star-three-points' size={60} color={COLORS.black} />
+            <MaterialCommunityIcons style={{ zIndex: 2, position: 'absolute', top: 11 }} name='star-three-points' size={40} color='#fcc449' />
+        </>
+    );
+
+    // Semi-popular posts.
+    if (score < 70) return (
+        <>
+            <MaterialCommunityIcons style={{ zIndex: 1, position: 'absolute' }} name='star-four-points' size={60} color={COLORS.black} />
+            <MaterialCommunityIcons style={{ zIndex: 2, position: 'absolute' }} name='star-four-points' size={40} color='#fc7735' />
+        </>
+    );
+
+    // Popular posts.
+    return (
+        <>
+            <MaterialCommunityIcons style={{ zIndex: 1, position: 'absolute' }} name='star' size={60} color={COLORS.black} />
+            <MaterialCommunityIcons style={{ zIndex: 2, position: 'absolute' }} name='star' size={40} color='#ff5340' />
+        </>
+    );
+}
+
+
+
+const styles = StyleSheet.create({
+    container: {
+        position: 'relative',
+        width: 60,
+        height: 60,
+        justifyContent: 'center',
+        alignItems: 'center'
+    }
+});
