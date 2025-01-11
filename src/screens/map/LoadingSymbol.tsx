f10c7502685f14a3ce285998788f85472fa93133
GSDV <sdvgab@gmail.com>
2025-01-10 17:27:43 -0800

    added loading indicator for map

--- src/screens/map/LoadingSymbol.tsx
+++ src/screens/map/LoadingSymbol.tsx
@@ -1,11 +1,18 @@
-import { View, ActivityIndicator } from 'react-native';
+import { ActivityIndicator } from 'react-native';
+
+import { useSafeAreaInsets } from 'react-native-safe-area-context';
 
 import { COLORS } from '@util/global-client';
-import { useSafeAreaInsets } from 'react-native-safe-area-context';
 
 
 
 export default function LoadingSymbol() {
     const insets = useSafeAreaInsets();
-    return <ActivityIndicator style={{ position: 'absolute', top: insets.top+5, right: 5 }} size='large' color={COLORS.primary} />;
+    return (
+        <ActivityIndicator
+            style={{ position: 'absolute', top: insets.top+5, right: 5 }}
+            size='large'
+            color={COLORS.primary}
+        />
+    );
 }
