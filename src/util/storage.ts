252d7830632f06ea9340e4e02f95f61ca02214ed
GSDV <sdvgab@gmail.com>
2025-01-11 11:21:32 -0800

    Subsituted AsyncStorage for SecureStore

--- src/util/storage.ts
+++ src/util/storage.ts
@@ -1,14 +1,14 @@
-import AsyncStorage from '@react-native-async-storage/async-storage';
+import * as SecureStore from 'expo-secure-store';
 
 import { AUTH_TOKEN_COOKIE_KEY } from './global';
 
 
 
 export const getAuthCookie = async () => {
-    const authToken = await AsyncStorage.getItem(AUTH_TOKEN_COOKIE_KEY);
+    const authToken = await SecureStore.getItemAsync(AUTH_TOKEN_COOKIE_KEY);
     return authToken;
 }
 
 export const setAuthCookie = async (val: string) => {
-    await AsyncStorage.setItem(AUTH_TOKEN_COOKIE_KEY, val);
+    await SecureStore.setItemAsync(AUTH_TOKEN_COOKIE_KEY, val);
 }
