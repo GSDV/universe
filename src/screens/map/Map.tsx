52bf06cd173e42ee1b8c6cb48df6f9fc34a1064a
GSDV <sdvgab@gmail.com>
2025-01-13 11:36:58 -0800

    Reworked map to avoid improper marker rendering; used cluster maps

--- src/screens/map/Map.tsx
+++ src/screens/map/Map.tsx
@@ -1,13 +1,47 @@
-import { useState, useRef, useEffect } from 'react';
+/*
+Both react native maps seem to be broken by SDK 52
+
+MapView from 'react-native-maps'
+- Will fetch but not display the most recent markers.
+- Displays markers from the last region instead.
+- Setting a key to the map displays current posts, but forces the map to refresh every time.
+- Setting a key to a React fragment causes crashes when moving.
+
+MapClusteredView from 'react-native-map-clustering'
+- Crashes when updating markers.
+
+Solution:
+- MapClusteredView but with a fragment. For some reason this works.
+- Cannot do clustering, however.
+*/
+/*
+Both react native maps seem to be broken by SDK 52
+
+MapView from 'react-native-maps'
+- Will fetch but not display the most recent markers.
+- Displays markers from the last region instead.
+- Setting a key to the map displays current posts, but forces the map to refresh every time.
+- Setting a key to a React fragment causes crashes when moving.
+
+MapClusteredView from 'react-native-map-clustering'
+- Crashes when updating markers.
+
+Solution:
+- MapClusteredView but with a fragment. For some reason this works.
+- Cannot do clustering, however.
+*/
+
+import React, { useState, useRef, useEffect } from 'react';
 
 import { StyleSheet, View, Animated, Dimensions } from 'react-native';
 
 import { useOperation } from '@providers/OperationProvider';
 
-import MapView, { Region, Marker } from 'react-native-maps';
+import MapClusteredView from 'react-native-map-clustering';
+import { Region, Marker } from 'react-native-maps';
 
-import PostMarker from './PostMarker';
 import PostPreview from './PostPreview';
+import PostMarker from './PostMarker';
 import LoadingSymbol from './LoadingSymbol';
 
 import { COLORS } from '@util/global-client';
@@ -31,13 +65,40 @@ const DEFAULT_REGION = {
 export default function Map() {
     const operationContext = useOperation();
 
-    const mapRef = useRef<MapView>(null);
-    const [currentRegion, setCurrentRegion] = useState<Region>(DEFAULT_REGION);
+    const mapRef = useRef<MapClusteredView>(null);
+    const [mapRegion, setMapRegion] = useState<Region>(DEFAULT_REGION);
 
     const [loading, setLoading] = useState<boolean>(false);
     const [posts, setPosts] = useState<PostType[]>([]);
     const [selectedPost, setSelectedPost] = useState<PostType | null>(null);
     const slideAnim = useRef(new Animated.Value(0)).current;
+
+    const fetchPosts = async () => {
+        const screen_bl = {
+            lat: mapRegion.latitude - (mapRegion.latitudeDelta/2),
+            lng: mapRegion.longitude - (mapRegion.longitudeDelta/2) 
+        };
+        const screen_tr = {
+            lat: mapRegion.latitude + (mapRegion.latitudeDelta/2),
+            lng: mapRegion.longitude + (mapRegion.longitudeDelta/2)
+        };
+        const screenCoords = JSON.stringify({ screen_bl, screen_tr });
+
+        const params = new URLSearchParams({ screenCoords });
+        const resJson = await fetchWithAuth(`map?${params}`, 'GET');
+        if (resJson.cStatus == 200) {
+            setPosts(resJson.posts);
+        }
+        setLoading(false);
+    }
+
+    useEffect(() => {
+        fetchPosts();
+    }, [mapRegion]);
+
+    const handleChangeRegion = (newRegion: Region) => {
+        setMapRegion(newRegion);
+    }
 
     const moveToUser = async () => {
         const { granted, location } = await requestLocation();
@@ -49,31 +110,16 @@ export default function Map() {
             latitudeDelta: 0.02,
             longitudeDelta: 0.02,
         };
-        setCurrentRegion(newRegion);
-        mapRef.current?.animateToRegion(newRegion, 500);
+        // TypeScript says "animateToRegion" does not exist on MapView, but it does.
+        // Ignore error with any.
+        (mapRef.current as any).animateToRegion(newRegion, 500);
+        handleChangeRegion(newRegion);
     }
 
     useEffect(() => {
         moveToUser();
     }, []);
 
-    const handleChangeRegion = async (newRegion: Region) => {
-        setLoading(true);
-        const screen_bl = {
-            lat: newRegion.latitude - (newRegion.latitudeDelta/2),
-            lng: newRegion.longitude - (newRegion.longitudeDelta/2) 
-        };
-        const screen_tr = {
-            lat: newRegion.latitude + (newRegion.latitudeDelta/2),
-            lng: newRegion.longitude + (newRegion.longitudeDelta/2)
-        };
-        const screenCoords = JSON.stringify({ screen_bl, screen_tr });
-
-        const params = new URLSearchParams({ screenCoords });
-        const resJson = await fetchWithAuth(`map?${params}`, 'GET');
-        if (resJson.cStatus == 200) setPosts(resJson.posts);
-        setLoading(false);
-    }
 
     const openPreview = (post: PostType) => {
         setSelectedPost(post);
@@ -108,31 +154,38 @@ export default function Map() {
         if (operationContext.lastOperation) setPosts(prev => operationContext.conductOperation(prev, 'map'));
     }, [operationContext.lastOperation]);
 
+
     return (
         <View style={styles.container}>
-            <MapView
+            <MapClusteredView
                 ref={mapRef}
                 style={styles.map}
-                initialRegion={currentRegion}
+                initialRegion={mapRegion}
+                onRegionChange={() => {if (!loading) setLoading(true);}}
                 onRegionChangeComplete={handleChangeRegion}
                 showsUserLocation
                 mapType='standard'
                 onPress={handleMapPress}
             >
-                {posts.map((p) => {
-                    if (p.lat === null || p.lng === null) return null;
-                    return (
-                        <Marker 
-                            key={p.id}
-                            coordinate={{ latitude: p.lat, longitude: p.lng }}
-                            zIndex={p.likeCount}
-                            onPress={(e) => e.stopPropagation()}
-                        >
-                            <PostMarker post={p} onPress={() => openPreview(p)} />
-                        </Marker>
-                    );
-                })}
-            </MapView>
+                <React.Fragment>
+                    {posts.map((p) => {
+                        const lat = p.lat ? Number(p.lat) : null;
+                        const lng = p.lng ? Number(p.lng) : null;
+                        if (lat === null || lng === null) return null;
+                        return (
+                            <Marker 
+                                key={p.id}
+                                coordinate={{ latitude: lat, longitude: lng }}
+                                zIndex={p.likeCount}
+                                onPress={(e) => e.stopPropagation()}
+                                tracksViewChanges={false}
+                            >
+                                <PostMarker post={p} onPress={() => openPreview(p)} />
+                            </Marker>
+                        );
+                    })}
+                </React.Fragment>
+            </MapClusteredView>
 
             {selectedPost && (
                 <Animated.View
