b46d022c9e167ef9198d1ec8e90caffb7ec4ef9b
GSDV <sdvgab@gmail.com>
2025-01-12 17:55:24 -0800

    GoBackFromPostHeader

--- src/components/GoBackHeader.tsx
+++ src/components/GoBackHeader.tsx
@@ -1,12 +1,13 @@
 import { TouchableOpacity, View } from 'react-native';
 
-import { useRouter } from 'expo-router';
+import { useLocalSearchParams, useRouter } from 'expo-router';
 
 import { Ionicons } from '@expo/vector-icons';
 
 import { SafeAreaTop } from '@components/SafeArea';
 
 import { COLORS } from '@util/global-client';
+import { usePostStore } from '@providers/PostStoreProvider';
 
 
 
@@ -20,5 +21,28 @@ export default function GoBackHeader() {
                 <Ionicons name='chevron-back' size={25} color={COLORS.primary} />
             </TouchableOpacity>
         </View>
-    )
+    );
+}
+
+
+
+export function GoBackFromPostHeader() {
+    const router = useRouter();
+
+    const postContext = usePostStore();
+    const postId = useLocalSearchParams().postId as string;
+
+    const goBack = () => {
+        postContext.removePost(postId);
+        router.back();
+    }
+
+    return (
+        <View>
+            <SafeAreaTop />
+            <TouchableOpacity style={{ paddingLeft: 5, width: 50 }} onPress={goBack}>
+                <Ionicons name='chevron-back' size={25} color={COLORS.primary} />
+            </TouchableOpacity>
+        </View>
+    );
 }
