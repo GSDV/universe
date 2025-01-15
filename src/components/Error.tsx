5979f46f053d513ce7baba84d2044cf9e93fff1d
GSDV <sdvgab@gmail.com>
2025-01-15 12:46:16 -0800

    SomethingWentWrong component for general errors

---
+++ src/components/Error.tsx
@@ -0,0 +1,11 @@
+import { View, Text } from 'react-native';
+
+
+
+export default function SomethingWentWrong() {
+    return (
+        <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+            <Text style={{ textAlign: 'center' }}>Something went wrong</Text>
+        </View>
+    )
+}
