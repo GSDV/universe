7d7d1ccffdf82b7b4ce4e155ad92a204c8ae4228
GSDV <sdvgab@gmail.com>
2025-01-16 20:29:57 -0800

    Splitting file

--- src/screens/post/View/Thread.tsx
+++ src/screens/post/View/Thread.tsx
@@ -1,42 +1,20 @@
-import React, { useRef, useState } from 'react';
+import React, { useRef } from 'react';
 
-import {
-    Text,
-    View,
-    StyleSheet,
-    TouchableOpacity,
-    Pressable,
-    NativeSyntheticEvent,
-    NativeScrollEvent
-} from 'react-native';
+import { Text, View, NativeSyntheticEvent, NativeScrollEvent } from 'react-native';
 
 import { FlatList } from 'react-native-gesture-handler';
 import { useRouter } from 'expo-router';
 
 import { usePostStore } from '@providers/PostStore';
 
-import { MaterialCommunityIcons } from '@expo/vector-icons';
-
-import Pfp from '@components/Pfp';
-import TextContent from '@components/post/TextContent';
-import Info from '@components/post/Info';
-import PostActionsMenu from '@components/post/Actions';
-import { DisplayMedia } from '@components/post/media/Display';
+import { AncestorPost, FocusPost, THREAD_LINE_WIDTH } from './Post'
 import { FeedPost } from '@components/post/FeedPost';
 import { Loading } from '@components/Loading';
 
 import { COLORS, FONT_SIZES } from '@util/global-client';
 
-import { PostType } from '@util/types';
+import { PostType } from '@util/types';;
 
-
-
-const THREAD_LINE_WIDTH = 3;
-
-const PFP_TOP_MARGIN = 10;
-const PFP_SIZE = 40;
-
-const LEFT_COLUMN_WIDTH = PFP_SIZE + 10;
 
 
 interface RenderPostType extends PostType {
@@ -70,8 +48,8 @@ export default function Thread({
     moreRepliesAvailable
 }: ThreadProps) {
     const router = useRouter();
+
     const flatListRef = useRef<FlatList>(null);
-    const [isNearTop, setIsNearTop] = useState(false);
 
     const addPost = usePostStore(state => state.addPost);
     
@@ -90,16 +68,13 @@ export default function Thread({
     // Handle scroll events to detect when we're near the top
     const handleScroll = (event: NativeSyntheticEvent<NativeScrollEvent>) => {
         const offsetY = event.nativeEvent.contentOffset.y;
-        const shouldLoadAncestors = offsetY < 100 && !loadingAncestors && 
-            focusPost.replyToId !== null && ancestors.length === 0;
-        
-        console.log(offsetY, shouldLoadAncestors);
-        // if (shouldLoadAncestors && !isNearTop) {
-        //     setIsNearTop(true);
-        //     fetchAncestors();
-        // } else if (offsetY >= 100 && isNearTop) {
-        //     setIsNearTop(false);
-        // }
+        const shouldLoadAncestors = (
+            (offsetY < -25) && 
+            (!loadingAncestors) && 
+            (focusPost.replyToId !== null) && 
+            (ancestors.length === 0)
+        );
+        if (shouldLoadAncestors) fetchAncestors();
     };
 
     const renderableItems: RenderItemType[] = [
@@ -192,159 +167,3 @@ function LoadingReplies() {
         </View>
     );
 }
-
-
-
-interface ThreadPostType {
-    post: PostType;
-    ownPost?: boolean;
-}
-
-type LineType = 'up' | 'down' | 'full' | 'none';
-
-
-
-
-// Direct ancestor to post that was pressed on.
-function AncestorPost({ post, ownPost, openAncestor }: { post: PostType, ownPost?: boolean, openAncestor: ()=>void }) {
-    const type = (!post.replyToId) ? 'down' : 'full';
-    return (
-        <TouchableOpacity onPress={openAncestor}>
-            <ThreadPost post={post} ownPost={ownPost} type={type} />
-        </TouchableOpacity>
-    );
-}
-
-
-
-// Post that was pressed on.
-function FocusPost({ post, ownPost }: { post: PostType, ownPost?: boolean }) {
-    const type = (!post.replyToId) ? 'none' : 'up';
-    return <ThreadPost post={post} ownPost={ownPost} type={type} />;
-}
-
-
-
-// "type" prop refers to the line on the left of the thread.
-function ThreadPost({ post, ownPost, type }: { post: PostType, ownPost?: boolean, type: LineType }) {
-    return (
-        <View style={{ paddingLeft: 5, width: '100%', flexDirection: 'row' }}>
-            <ThreadLine username={post.author.username} pfpKey={post.author.pfpKey} type={type} />
-
-            <View style={{ paddingVertical: 15, padding: 10, flex: 6, gap: 10 }}>
-                <PostHeader post={post} ownPost={ownPost} />
-
-                <TextContent post={post} truncate={false} />
-
-                <DisplayMedia media={post.media} />
-
-                <Info post={post} />
-            </View>
-        </View>
-    );
-}
-
-
-
-function ThreadLine({ username, pfpKey, type }: { username: string, pfpKey: string, type: LineType }) {
-    const router = useRouter();
-    const navigateToProfile = () => {
-        router.push({
-            pathname: '/profile/[username]/view',
-            params: { username }
-        });
-    }
-
-    return (
-        <View style={{ width: LEFT_COLUMN_WIDTH, alignItems: 'center' }}>
-            {(type === 'full') && <View style={[styles.line, styles.fullLine]} />}
-            {(type === 'up') && <View style={[styles.line, styles.upperLine]} />}
-            {(type === 'down') && <View style={[styles.line, styles.lowerLine]} />}
-            <Pressable onPress={navigateToProfile} style={styles.pfpContainer}>
-                <Pfp pfpKey={pfpKey} style={styles.pfp} />
-            </Pressable>
-        </View>
-    );
-}
-
-
-
-
-
-function PostHeader({ post, ownPost }: ThreadPostType) {
-    const router = useRouter();
-    const navigateToProfile = () => {
-        router.push({
-            pathname: '/profile/[username]/view',
-            params: { username: post.author.username }
-        });
-    }
-
-    return (
-        <Pressable onPress={navigateToProfile} style={{ width: '100%', flexDirection: 'row', alignItems: 'center', gap: 5 }}>
-            <View style={{ flex: 1, justifyContent: 'space-between' }}>
-                <View style={{ flexDirection: 'row', alignItems: 'center', gap: 2 }}>
-                    <Text style={styles.displayName} numberOfLines={1} ellipsizeMode='tail'>{post.author.displayName}</Text>
-                    {post.author.verified && <MaterialCommunityIcons name='star-four-points' style={{ fontSize: FONT_SIZES.m }} color={COLORS.primary} />}
-                </View>
-                <Text style={styles.username} numberOfLines={1} ellipsizeMode='tail'>@{post.author.username}</Text>
-            </View>
-
-            {/* Spacer */}
-            <View style={{ width: 10 }} />
-
-            <PostActionsMenu post={post} ownPost={ownPost} />
-        </Pressable>
-    );
-}
-
-
-
-const styles = StyleSheet.create({
-    header: {
-        flex: 1,
-        width: '100%',
-        flexDirection: 'row',
-        alignItems: 'center',
-        gap: 5 
-    },
-    pfpContainer: {
-        top: PFP_TOP_MARGIN,
-        width: PFP_SIZE,
-        height: PFP_SIZE,
-        zIndex: 2
-    },
-    pfp: {
-        width: PFP_SIZE,
-        height: PFP_SIZE,
-        borderRadius: 50
-    },
-    displayName: {
-        color: COLORS.black,
-        fontSize: FONT_SIZES.m,
-        fontWeight: '500'
-    },
-    username: {
-        color: COLORS.gray,
-        fontSize: FONT_SIZES.m
-    },
-    line: {
-        position: 'absolute',
-        width: THREAD_LINE_WIDTH,
-        backgroundColor: COLORS.primary,
-        left: '50%',
-        marginLeft: -THREAD_LINE_WIDTH / 2
-    },
-    fullLine: {
-        top: 0,
-        bottom: 0
-    },
-    upperLine: {
-        top: 0,
-        height: PFP_TOP_MARGIN + PFP_SIZE/2
-    },
-    lowerLine: {
-        top: PFP_TOP_MARGIN + PFP_SIZE/2,
-        bottom: 0
-    }
-});
