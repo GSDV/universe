a59313f95b3b502366338e645c0f3b3e923a02da
GSDV <sdvgab@gmail.com>
2025-01-19 09:53:09 -0800

    Root layout clean

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -3,17 +3,19 @@ import { useEffect } from 'react';
 import { Stack } from 'expo-router';
 import { useFonts } from 'expo-font';
 
+import { GestureHandlerRootView } from 'react-native-gesture-handler';
+
 import Providers from '@providers/Providers';
 
-import { GestureHandlerRootView } from 'react-native-gesture-handler';
 import * as SplashScreen from 'expo-splash-screen';
 
 import { COLORS } from '@util/global-client';
 
 
 
-// Prevent the splash screen from auto-hiding before asset loading is complete.
 SplashScreen.preventAutoHideAsync();
+
+
 
 const contentStyle = { backgroundColor: COLORS.background };
 
@@ -21,7 +23,7 @@ const contentStyle = { backgroundColor: COLORS.background };
 
 export default function RootLayout() {
     const [loaded] = useFonts({
-        Helvetica: require('../src/assets/fonts/HelveticaNeue/Regular.ttf') // SpaceMono-Regular
+        Helvetica: require('../src/assets/fonts/HelveticaNeue/Regular.ttf')
     });
 
     useEffect(() => {
@@ -31,25 +33,17 @@ export default function RootLayout() {
     if (!loaded) return null;
 
     return (
-        <GestureHandlerRootView>
+        <GestureHandlerRootView style={{ flex: 1 }}>
             <Providers>
                 <Stack initialRouteName='(tabs)'>
                     <Stack.Screen name='index' options={{ headerShown: false, contentStyle }} />
-
                     <Stack.Screen name='(tabs)' options={{ headerShown: false, animation: 'fade', gestureEnabled: false, contentStyle }} />
-
-                    <Stack.Screen name='profile/[username]/view' options={{ headerShown: false, contentStyle }} getId={({ params }) => params?.username} />
-
                     <Stack.Screen name='settings/index' options={{ headerShown: false, contentStyle }} />
                     <Stack.Screen name='settings/profile-edit/index' options={{ headerShown: false, contentStyle }} />
-
                     <Stack.Screen name='login/index' options={{ headerShown: false, contentStyle }} />
                     <Stack.Screen name='signup/index' options={{ headerShown: false, contentStyle }} />
                     <Stack.Screen name='signup/verification/index' options={{ headerShown: false, contentStyle }} />
-
                     <Stack.Screen name='post/create' options={{ headerShown: false, contentStyle }} />
-                    <Stack.Screen name='post/[postId]/view' options={{ headerShown: false, contentStyle }} getId={({ params }) => params?.viewId} />
-
                     <Stack.Screen name='+not-found' options={{ headerShown: false, contentStyle }} />
                 </Stack>
             </Providers>
