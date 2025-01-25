a2009b700a2b2170c5eb2187deffadfbdcaefb15
GSDV <sdvgab@gmail.com>
2025-01-25 16:30:27 -0600

    Link fix

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -43,7 +43,6 @@ export default function RootLayout() {
                     <Stack.Screen name='login/index' options={{ headerShown: false, contentStyle }} />
                     <Stack.Screen name='signup/index' options={{ headerShown: false, contentStyle }} />
                     <Stack.Screen name='signup/verification/index' options={{ headerShown: false, contentStyle }} />
-                    <Stack.Screen name='post/create' options={{ headerShown: false, contentStyle }} />
                     <Stack.Screen name='reset-password/index' options={{ headerShown: false, contentStyle }} />
                     <Stack.Screen name='+not-found' options={{ headerShown: false, contentStyle }} />
                 </Stack>
