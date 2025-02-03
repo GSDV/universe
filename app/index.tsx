e07127b28051ee8c04030aaa1047a8f3b234f872
GSDV <sdvgab@gmail.com>
2025-02-03 12:23:14 -0600

    App version checking

--- app/index.tsx
+++ app/index.tsx
@@ -1,16 +1,20 @@
 import { useEffect, useState } from 'react';
 
-import { View } from 'react-native';
+import { Linking, View, Alert as AlertPopUp, Text } from 'react-native';
 
 import { useRouter } from 'expo-router';
 
+import { useUser } from '@providers/UserProvider';
+
 import NotLoggedIn from '@screens/NotLoggedIn';
 
-import { useUser } from '@providers/UserProvider';
+import Button from '@components/Button';
 import { SafeAreaTop } from '@components/SafeArea';
 import { CheckIfLoading } from '@components/Loading';
 
-import { fetchWithAuth } from '@util/fetch';
+import { COLORS, FONT_SIZES } from '@util/global-client';
+
+import { fetchBasic, fetchWithAuth } from '@util/fetch';
 import { getAuthCookie } from '@util/storage';
 
 
@@ -20,10 +24,35 @@ export default function Index() {
     const router = useRouter();
     const userContext = useUser();
 
+    const [outDated, setOutDated] = useState<boolean>(false);
     const [loading, setLoading] = useState<boolean>(true);
+
+    const goToAppStore = async () => {
+        try {
+            await Linking.openURL(`itms-apps:///apps.apple.com/app/id/6741363295`);
+        }
+        catch (e) {
+            AlertPopUp.alert('Unable to open App Store', '', [{ text: 'Ok', onPress: () => {} }]);
+        }
+    }
+
+    const promptUpdate = () => {
+        AlertPopUp.alert('Update Available!', 'A new version of this app is available. Please update it in the App Store.', [
+            { text: 'Ok', onPress: () => {} },
+            { text: 'Go to App Store', onPress: goToAppStore },
+        ]);
+    }
 
     const loadProfile = async () => {
         setLoading(true);
+
+        // Check if user is using an up-to-date version of the app.
+        const resJsonVersion = await fetchBasic(`version`, 'GET');
+        if (resJsonVersion.cStatus == 150) {
+            promptUpdate();
+            setOutDated(true);
+            return;
+        }
 
         const authTokenCookie = await getAuthCookie();
         if (!authTokenCookie) {
@@ -31,7 +60,7 @@ export default function Index() {
             return;
         }
 
-        const resJson = await fetchWithAuth('user', 'GET');
+        const resJson = await fetchWithAuth(`user`, 'GET');
         if (resJson.cStatus == 200) {
             const user = resJson.user;
             userContext.setUser(user);
@@ -50,9 +79,18 @@ export default function Index() {
     return (
         <View style={{ flex: 1 }}>
             <SafeAreaTop />
-            <CheckIfLoading loading={loading}>
-                <NotLoggedIn />
-            </CheckIfLoading>
+                {outDated ?
+                    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+                        <View style={{ gap: 20 }}>
+                            <Text style={{ textAlign: 'center', color: COLORS.primary, fontSize: FONT_SIZES.l }}>Please update the app to continue</Text>
+                            <Button onPress={goToAppStore}>Go to App Store</Button>
+                        </View>
+                    </View>
+                :
+                    <CheckIfLoading loading={loading}>
+                        <NotLoggedIn />
+                    </CheckIfLoading>
+                }
         </View>
     );
 }
