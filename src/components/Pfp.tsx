86c7d19fcf748ab1e18f4042bbb39a25f2322b6b
GSDV <sdvgab@gmail.com>
2025-03-01 10:59:58 -0600

    Adding Fragment

--- src/components/Pfp.tsx
+++ src/components/Pfp.tsx
@@ -1,3 +1,5 @@
+import { Fragment } from 'react';
+
 import { Image, ImageStyle, StyleProp } from 'react-native';
 
 import { COLORS, DEFAULT_PFP, imgUrl } from '@util/global-client';
@@ -6,10 +8,12 @@ import { COLORS, DEFAULT_PFP, imgUrl } from '@util/global-client';
 
 export default function Pfp({ pfpKey, style }: { pfpKey: string, style?: StyleProp<ImageStyle> }) {
     return (
-        <>{pfpKey=='' ? 
-            <Image style={[{backgroundColor: COLORS.background}, style]} source={DEFAULT_PFP} />
-        :
-            <Image style={[{backgroundColor: COLORS.background}, style]} source={{ uri: imgUrl(pfpKey) }} />
-        }</>
+        <Fragment>
+            {pfpKey=='' ? 
+                <Image style={[{backgroundColor: COLORS.background}, style]} source={DEFAULT_PFP} />
+            :
+                <Image style={[{backgroundColor: COLORS.background}, style]} source={{ uri: imgUrl(pfpKey) }} />
+            }
+        </Fragment>
     );
 }
