341f863ed42b14536af5e2a1f31c9a83bf2ecffd
GSDV <sdvgab@gmail.com>
2025-03-17 14:44:59 -0500

    Location util now can only request permissions, not location position itself

--- src/util/location.ts
+++ src/util/location.ts
@@ -1,28 +1,8 @@
-import { Alert as AlertPopUp } from 'react-native';
-
 import * as Location from 'expo-location';
 
 
 
-export const requestLocation = async () => {
+export const requestLocationPermission = async () => {
     const { status } = await Location.requestForegroundPermissionsAsync();
-    if (status !== 'granted') {
-        AlertPopUp.alert(
-            "Permission Required",
-            "To use the map, this app needs access to your location.",
-            [
-                {
-                    text: "Ok",
-                    style: "cancel",
-                    onPress: () => {}
-                }
-            ]
-        );
-        return { granted: false, location: null };
-    }
-
-    let location = await Location.getLastKnownPositionAsync({ maxAge: 5000 });
-    if (!location) location = await Location.getCurrentPositionAsync({ accuracy: Location.Accuracy.Highest });
-
-    return { granted: true, location };
+    return (status === 'granted');
 }
